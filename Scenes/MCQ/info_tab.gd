f7a2c0cc91d7f4ebc482ea5d465202907281b8c2
concerned-doggo <codeformetoo@proton.me>
2024-09-18 22:49:24 +0530

    update level 3 and refactor info-tab with exported label

--- Scenes/MCQ/info_tab.gd
+++ Scenes/MCQ/info_tab.gd
@@ -1,26 +1,20 @@
 extends Area2D
 
+@export var label : Label
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
-@onready var label = $Label
-
+	z_index = -1
 
 func _on_body_entered(body):
 	if body.is_in_group("player"):
+		print("gone")
+		label.visible = true
 		# var infoLabel = Label.new()
 		label.add_theme_font_override("font", load("res://Assets/fonts/PixelOperator8.ttf")) 
 		label.add_theme_font_size_override("font_size", 8) 
 		#$Label.autowrap_mode = TextServer.AutowrapMode.AUTOWRAP_WORD
 
 
-# func _on_body_exited(body:Node2D):
-# 	if body.is_in_group("player") :
-# 		print("gone")
+func _on_body_exited(body:Node2D):
+	if body.is_in_group("player"):
+		label.visible = false
